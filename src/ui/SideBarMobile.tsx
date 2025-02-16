073080f2511a916b6f8cf214347ed5973c21fae5
hamid-ensafi <hamidensafi1380.a@gmail.com>
2025-02-16 13:21:32 +0330

    test

--- src/ui/SideBarMobile.tsx
+++ src/ui/SideBarMobile.tsx
@@ -1,35 +1,36 @@
-import Window from "@/Components/window/Window";
-import SideBarContent from "@/ui/SideBarContent";
-import { closeWindow } from "@/Components/window/slice";
-import { EssetionalAddOutline, EssetionalMenuOutline } from "react-icons-sax";
-import Button from "./Button";
-import { useDispatch } from "react-redux";
-import Avatar from "./Avatar";
+// import Window from "@/Components/window/Window";
+// import SideBarContent from "@/ui/SideBarContent";
+// import { closeWindow } from "@/Components/window/slice";
+// import { EssetionalAddOutline, EssetionalMenuOutline } from "react-icons-sax";
+// import Button from "./Button";
+// import { useDispatch } from "react-redux";
+// import Avatar from "./Avatar";
 
 function SideBarMobile(): React.JSX.Element {
-  const dispath = useDispatch();
+  // const dispath = useDispatch();
 
   return (
-    <Window>
-      <>
-        <Window.Selection id="1" type="simple">
-          <EssetionalMenuOutline />
-        </Window.Selection>
-        <Window.Content direction="left" id="1">
-          <div className="h-full p-4 md:hidden  bg-white">
-            <div className="flex flex-col gap-4 bg-grayprime-100   h-full px-4">
-              <div className="flex border-b border-grayprime-300  justify-between py-6 ">
-                <Avatar direction="reverse" />
-                <Button onClick={() => dispath(closeWindow())} type="close">
-                  <EssetionalAddOutline size={32} />
-                </Button>
-              </div>
-              <SideBarContent />
-            </div>
-          </div>
-        </Window.Content>
-      </>
-    </Window>
+    <></>
+    // <Window>
+    //   <>
+    //     <Window.Selection id="1" type="simple">
+    //       <EssetionalMenuOutline />
+    //     </Window.Selection>
+    //     <Window.Content direction="left" id="1">
+    //       <div className="h-full p-4 md:hidden  bg-white">
+    //         <div className="flex flex-col gap-4 bg-grayprime-100   h-full px-4">
+    //           <div className="flex border-b border-grayprime-300  justify-between py-6 ">
+    //             <Avatar direction="reverse" />
+    //             <Button onClick={() => dispath(closeWindow())} type="close">
+    //               <EssetionalAddOutline size={32} />
+    //             </Button>
+    //           </div>
+    //           <SideBarContent />
+    //         </div>
+    //       </div>
+    //     </Window.Content>
+    //   </>
+    // </Window>
   );
 }
 
